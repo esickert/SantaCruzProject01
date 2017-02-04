1f3ba958cfb3e451a433214363b91c7dd2a47da1
Erich Sickert <esickert@gmail.com>
2017-02-03 17:43:48 -0800

    plus

--- src/test/java/com/esickert/testcases/Chap7Test.java
+++ src/test/java/com/esickert/testcases/Chap7Test.java
@@ -1,8 +1,11 @@
 package com.esickert.testcases;
 
 import org.junit.Test;
-import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
+
+
+
 /**
  * Created by esickert on 2/1/2017.
  * Chapter 7 code examples
@@ -23,7 +26,7 @@ public class Chap7Test {
     System.out.println(("Are these equal? " + (sum(a, b) == 11)));    //INCORRECT STATEMENT!!!!!!
     System.out.println(this.b);  //note this returns 44 which is a class field, not local variable
     assertTrue("Are these equal???",sum(a,$b) == 11);
-    assertEquals(10.0F,lowed,0);                // I don't understand this notation.
+    assertEquals(10.0F, lowed,0);                // I don't understand this notation.
     assertEquals("bob",$aString);
 
     }
@@ -32,4 +35,64 @@ public class Chap7Test {
 
     return x + y;
     }
+
+    @Test
+    public void BooleanType()   {     //PAGE 91 JAVA FOR TESTERS
+        boolean truth = true;
+        boolean falsey = false;
+        long bigNum= 900_000_000;  //NOTE can use underscore for readability
+
+
+        assertTrue(truth);
+        assertFalse(falsey);
+
+        //*************************
+
+        truth = Boolean.TRUE;
+        falsey = Boolean.FALSE;
+
+        assertTrue(truth);
+        assertFalse(falsey);
+//        assertFalse(truth);
+
+        System.out.println(bigNum);
+
+     }
+
+     @Test
+     public void FloatingPointType() {    //PAGE 97 JAVA FOR TESTERS
+
+     float singlePercision32bit;
+     double doublePercision64bit;
+
+     //SoftAssert soft = testCase.getSoftAssert();
+
+     System.out.println("* 'float' range: " +
+                            Float.MIN_VALUE + " to " +
+                            Float.MAX_VALUE);
+
+     System.out.println("* 'double' range : " +
+                            Double.MIN_VALUE + " to " +
+                            Double.MAX_VALUE);
+
+     singlePercision32bit = 10.0F; //suffix F to get a float
+     assertEquals(10F, singlePercision32bit, 0);
+
+     doublePercision64bit = 20.0;
+     assertEquals(20D, doublePercision64bit, 0);  //not suffix D for double
+
+     }
+
+     @Test
+     public void ternaryOperatorsExplored() {
+
+        int x;
+        x = (4>3) ?  2 : 1;
+        System.out.println(x);
+
+        assertEquals(x, 2);
+
+        assertTrue( (5 >= 4) ? true : false);
+    }
+
 }
