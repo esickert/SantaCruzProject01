bfb0f4c9f2fcebcc8424ce0fd7633336f9514f3b
Erich Sickert <esickert@gmail.com>
2017-03-15 17:39:40 -0700

    plus

---
+++ src/test/java/com/esickert/testcases/Test10.java
@@ -0,0 +1,54 @@
+package com.esickert.testcases;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by esickert on 3/13/2017.
+ */
+public class Test10 {
+
+    @Test public void simpleCollectionEx()  {
+        String[] numbers = {"zero", "one", "two", "three"};
+        List<String> numbers0123 = Arrays.asList(numbers);     //simple example of collections
+
+        for(String temp:numbers0123)    {
+            System.out.println(temp);
+        }
+
+        assertEquals("zero", numbers0123.get(0));
+//        assertEquals("is two equal to 2???","two", numbers0123.get(3));
+        numbers0123.set(0, "one");//in List(index, element);
+        System.out.println(numbers0123.get(0) + " line 26"); //******************using set here because add is not allowed as umbers0123 is an array.
+
+        //Note this is jumping right into collections using lists. Here we are doing something that i don't understand.
+
+    } //end of simpleCollections
+
+    @Test
+    public void dynamicCollection() {
+
+        List<String> num0123 = new ArrayList<String>();
+
+        num0123.add("zero");
+        num0123.add("one");
+        num0123.add("two");
+        num0123.add("three");
+
+        for(String temp:num0123)    {
+            System.out.println(temp + " ");
+        }
+
+        assertEquals("two", num0123.get(2));
+        assertEquals("three", num0123.get(3));
+
+       List<String> game = new ArrayList<String>();  //List is an interface. Cannot instatiate an interface, only classes can be instantiated.
+    }
+
+
+
+}  //end of Test10
