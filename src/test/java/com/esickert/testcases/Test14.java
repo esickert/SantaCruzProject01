9ac68ae81fbb01a5c19394161f52b31be3513b29
Erich Sickert <esickert@gmail.com>
2017-05-18 17:39:51 -0700

    plus

--- src/test/java/com/esickert/testcases/Test14.java
+++ src/test/java/com/esickert/testcases/Test14.java
@@ -1,6 +1,9 @@
 package com.esickert.testcases;
 
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Created by esickert on 5/17/2017.
@@ -16,8 +19,20 @@ public class Test14 {
         }
     }
 
+
+    @Ignore
     @Test(expected = ArithmeticException.class)  //if you comment this out the method will throw exception (fail).
     public void testDivisionWithException()    {
         int x = 1 / 0;
     }
+
+
+    @Rule
+    public ExpectedException expected =  ExpectedException.none(); //don't understand this yet!!!
+    @Ignore
+    @Test
+    public void testDivisionWithRule()  {
+        int x = 1 / 0;
+    }
+
 }
