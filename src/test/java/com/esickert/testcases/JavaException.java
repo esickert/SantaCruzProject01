ac2094453db5646bf138b3bf1779d39f5882bdd1
Erich Sickert <esickert@gmail.com>
2017-06-27 17:33:14 -0700

    plus

---
+++ src/test/java/com/esickert/testcases/JavaException.java
@@ -0,0 +1,15 @@
+package com.esickert.testcases;
+
+/**
+ * Created by esickert on 5/17/2017.
+ */
+public class JavaException extends Exception    {
+
+    JavaException() {
+        super();
+    }
+
+    JavaException(String message)   {
+        super(message);
+    }
+}
