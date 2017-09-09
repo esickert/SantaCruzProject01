0ae375febf0e06f15dd16bfd93d872e0a58e1b89
Erich Sickert <esickert@gmail.com>
2017-09-08 17:12:21 -0700

    plus

---
+++ src/main/java/ForTestingOnly.java
@@ -0,0 +1,7 @@
+public class ForTestingOnly {
+    public static void main(String[] args)  {
+
+        System.out.println("Hello, World!!");
+        System.out.println("Hello, It's me dodo!!!");
+    }
+}
