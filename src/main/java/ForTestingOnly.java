21151c2baca52d2924c3d7756f3b65175572d10b
Erich Sickert <esickert@gmail.com>
2017-09-11 17:20:44 -0700

    plus

--- src/main/java/ForTestingOnly.java
+++ src/main/java/ForTestingOnly.java
@@ -1,7 +1,11 @@
 public class ForTestingOnly {
     public static void main(String[] args)  {
-
+        int x = 0;
         System.out.println("Hello, World!!");
         System.out.println("Hello, It's me dodo!!!");
+        while (x < 5) {
+            System.out.println(x++);
+        }
+
     }
 }
