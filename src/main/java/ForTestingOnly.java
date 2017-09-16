ddddb1ce877adb90df87b2431bf293d4522a3e5d
Erich Sickert <esickert@gmail.com>
2017-09-15 17:34:53 -0700

    plus

--- src/main/java/ForTestingOnly.java
+++ src/main/java/ForTestingOnly.java
@@ -1,16 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
 public class ForTestingOnly {
     public static void main(String[] args)  {
-        int x = 0;
-        System.out.println("Hello, World!!");
-        System.out.println("Hello, It's me dodo!!!");
-        while (x < 5) {
-            System.out.print(++x + " ");
-        }
-        System.out.println("Letter from ");
+        String filename = "c:/tmp/numbers.dat";
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename));    //stream classes to output to 123
+            // binary files.
 
-        for(int i = 0; i <= 10; i++) {
-            System.out.print(i + " ");
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter non negative integers: ");
+            System.out.println("Place a negative integer at the end. ");
+
+            int anInteger;
+            do {
+                anInteger = keyboard.nextInt();
+                outputStream.writeInt(anInteger);  //write an integer to the binary file!!!!
+            } while (anInteger >= 0);
+
+            System.out.println("Numbers and sentinal value");
+            System.out.println("written to a file " + filename);
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR-File not found!!!");
+        } catch (IOException e) {   //used because <binary stream>.close() will throw a IOException. In contrast to a text file which will NOT.
+            System.out.println("Problem with output to file: " + filename);
         }
+
     }
 }
+
 
