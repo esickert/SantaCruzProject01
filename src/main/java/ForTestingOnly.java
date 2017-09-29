ba2dec7ee4089ddf665e90f78ee449483a8093ff
Erich Sickert <esickert@gmail.com>
2017-09-28 17:19:01 -0700

    plus

--- src/main/java/ForTestingOnly.java
+++ src/main/java/ForTestingOnly.java
@@ -1,12 +1,11 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class ForTestingOnly {
     public static void main(String[] args)  {
         String filename = "c:/tmp/numbers.dat";
+        String y = "";
+        String n = "";
         try {
             ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename));    //stream classes to output to 123
             // binary files.
@@ -18,16 +17,36 @@ public class ForTestingOnly {
             int anInteger;
             do {
                 anInteger = keyboard.nextInt();
-                outputStream.writeInt(anInteger);  //write an integer to the binary file!!!!
+                outputStream.writeInt(anInteger);  //write an integer to the binary file!!!!123456 -1
             } while (anInteger >= 0);
 
             System.out.println("Numbers and sentinal value");
             System.out.println("written to a file " + filename);
             outputStream.close();
+
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename));
+            int aNumber = inputStream.readInt();
+            System.out.println("Is this the number you read in??? "   + aNumber);
+            System.out.println("Type y for yes, n for no");
+            String x = "";
+            x = keyboard.next();
+            switch(x)   {
+                case "y":
+                    System.out.println("Fucking awesome");
+                    break;
+                case "n":
+                    System.out.println("Sorry, I fucked up!!!");
+                    break;
+                default  :
+                    System.out.println("invalid response fuckface!! BYE");
+            }
+
+            inputStream.close();
         } catch (FileNotFoundException e) {
             System.out.println("ERROR-File not found!!!");
         } catch (IOException e) {   //used because <binary stream>.close() will throw a IOException. In contrast to a text file which will NOT.
             System.out.println("Problem with output to file: " + filename);
+
         }
 
     }
