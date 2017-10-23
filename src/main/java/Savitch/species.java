18ee69b41c5098eb1e62d1d4751ca4392c451db0
Erich Sickert <esickert@gmail.com>
2017-10-23 16:16:17 -0700

    plus

---
+++ src/main/java/Savitch/species.java
@@ -0,0 +1,45 @@
+package Savitch;
+
+import org.junit.Test;
+
+import java.io.Serializable;
+
+public class Species implements Serializable{
+
+    private String name;
+    private int population;
+    private double growthRate;
+
+    public Species()    {
+
+        name = null;
+        population = 0;
+        growthRate = 0;
+    }
+//@Test
+    public Species(String initialName, int initialPopulation, double initialGrowthRate) {
+
+    name = initialName;
+    if (initialPopulation >= 0)
+        population = initialPopulation;
+//       population = initialPopulation;
+    else {
+        System.out.println("ERROR-negative population");
+        System.exit(0);
+    }
+    growthRate = initialGrowthRate;
+    }
+
+    @Override
+    public String toString() {
+        return ("Name = " + name + "\n" +
+                "Population = " + population + "\n" +
+                "Growth rate = " + growthRate + "%");
+    }
+
+
+
+
+
+}
+
