697625ac62e4b3efd182ebc61eb13da2ec3e4f62
Erich Sickert <esickert@gmail.com>
2017-10-30 17:40:55 -0700

    plus

--- src/test/java/com/esickert/testcases/SeleniumTest2.java
+++ src/test/java/com/esickert/testcases/SeleniumTest2.java
@@ -1,6 +1,32 @@
 package com.esickert.testcases;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Assert;
+
+
 public class SeleniumTest2 {
 
+    @Test
+    public void chromeTest() {
+        //memorize this and firefox too!!
+        System.setProperty("webdriver.chrome.driver","C://DriversForSelenium//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("http://www.bing.com");
+    }
+
+    @Test
+    public void firefoxTest()   {
+        System.setProperty("webdriver.gecko.driver","C://DriversForSelenium//geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        String url2 = "http:ww.yahoo.com";
+
+        driver.get("http:www.yahoo.com");
+
+        Assert.assertEquals(url2,("http:www.yahoo.com"));
+    }
 }
