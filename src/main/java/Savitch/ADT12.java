78055f459187374a509a357bcf7c730b0f5ccff5
Erich Sickert <esickert@gmail.com>
2017-11-09 14:32:31 -0800

    plus

--- src/main/java/Savitch/ADT12.java
+++ src/main/java/Savitch/ADT12.java
@@ -16,13 +16,13 @@ public class ADT12 {
             Scanner userInput = new Scanner(System.in);
             ArrayList<Integer> cat = new ArrayList<>(20); // new notation as of java 7
             //our arraylist has limit of 20 items;
-            System.out.println("Is this true " + cat.add(1));
-            cat.add(2);
+            //System.out.println("Is this true " + cat.add(1));
+            //cat.add(2);
 
             for (Integer temp : cat)
                 System.out.print("This will print out the contents of the arraylist " + temp + "  \n");
 
-            System.out.println(cat.set(1, 200));
+            //System.out.println(cat.set(1, 200));
             for (Integer temp : cat)
                 System.out.print("This will print out the contents of the arraylist " + temp + "  \n");
 
