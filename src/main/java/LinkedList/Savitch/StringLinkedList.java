4dab203bca0223f922503950e04b7b6c4ca50f2e
Erich Sickert <esickert@gmail.com>
2017-12-09 16:31:20 -0800

    plus

--- src/main/java/LinkedList/Savitch/StringLinkedList.java
+++ src/main/java/LinkedList/Savitch/StringLinkedList.java
@@ -3,4 +3,18 @@ package LinkedList.Savitch;
 public class StringLinkedList {
 
     private ListNode head;   //This is confusing
+
+    public StringLinkedList()   {
+        head= null;
+    }
+/**
+   displays data on the list
+*/
+     public void showList() {
+         ListNode position = head;
+         while (position != null)   {
+             System.out.println(position.getData());
+             position = position.getLink();
+         }
+     }//end of method showList()
 }
