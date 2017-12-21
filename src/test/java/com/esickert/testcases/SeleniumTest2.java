bafe8569d8d7da0cd717a4566788a1778cb8b09b
Erich Sickert <esickert@gmail.com>
2017-12-20 16:38:30 -0800

    plus

--- src/test/java/com/esickert/testcases/SeleniumTest2.java
+++ src/test/java/com/esickert/testcases/SeleniumTest2.java
@@ -1,12 +1,20 @@
 package com.esickert.testcases;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.Thread.sleep;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -41,5 +49,32 @@ public class SeleniumTest2 {
 //        assertThat(url2, is("http:www.yahoo.com")); //figure this out??????????????????????
         assertEquals(x,(3));
 //        assertThat(x,is(1));     //this doesn't work!!!!!!
+    }
+    @Test
+    public void desiredProperties() throws IOException, InterruptedException {
+
+        System.setProperty("webdriver.gecko.driver","c://DriversForSelenium//Geckodriver.exe");
+
+        Map capabilitiesMap = new HashMap();
+        capabilitiesMap.put("takeScreenShot", true);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities(capabilitiesMap);
+        WebDriver driver = new FirefoxDriver(capabilities);
+        driver.get("http://www.bing.com");
+        sleep(5000);
+        File fileName = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//        FileUtils.copyFile(fileName, new File("c:\\tmp\\screenshot.png"));
+        System.out.println(fileName.getAbsolutePath());
+        //********************************************************************************************************
+        try {
+            FileUtils.copyFile(fileName, new File("c:\\tmp\\screenshot.png"));
+        }
+        catch(IOException e)
+        {
+            System.out.println("ERROR");
+        }
+        //*******************************************************************************************************
+
+        assertEquals(fileName,"c:\\tmp\\screenshot.png");//this fails because the entire string coparison fails.
     }
 }
