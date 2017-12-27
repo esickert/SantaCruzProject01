29a64a557e5045b5327ec93170c181b2c3eb447f
Erich Sickert <esickert@gmail.com>
2017-12-26 16:09:30 -0800

    plus

--- src/main/java/LinkedList/Savitch/LinkedListtOfStrings.java
+++ src/main/java/LinkedList/Savitch/LinkedListtOfStrings.java
@@ -7,9 +7,14 @@ public class LinkedListtOfStrings {
 
         StringLinkedList linked = new StringLinkedList();
 
-        linked.addNodeToStart("Hello");
+        linked.addNodeToStart("Lions in the street and roaming");
+        linked.addNodeToStart("Dogs in heat rebid foaming");
+        linked.addNodeToStart("A beast caged in the heart of the city");
+        linked.addNodeToStart("Its' mother lying in the summer ground,");
+        linked.showList();
          //this will now reference a memory link to the next node.....
         System.out.println(linked.length());
         linked.showList();
+       
     }
 }
