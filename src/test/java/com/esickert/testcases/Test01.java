da48259247558a797dcaf53962b677a27c2049a7
Erich Sickert <esickert@gmail.com>
2018-01-20 20:57:18 -0800

    plus

---
+++ src/test/java/com/esickert/testcases/Test01.java
@@ -0,0 +1,13 @@
+package com.esickert.testcases;
+
+import org.junit.Test;
+
+public class Test01 {
+
+
+    @Test
+    public void test()  {
+
+        System.out.println("This is a test");
+    }
+}
