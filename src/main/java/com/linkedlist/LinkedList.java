b179263b8daa3450cd65d83e7340275f30bf0312
Erich Sickert <esickert@gmail.com>
2018-02-08 16:53:28 -0800

    plus

--- src/main/java/com/linkedlist/LinkedList.java
+++ src/main/java/com/linkedlist/LinkedList.java
@@ -2,18 +2,41 @@ package com.linkedlist;
 
 public class LinkedList {
 
-    LinkedList() {
+    private Node head;
+
+    LinkedList()    {
+        head = null;
     }
 
-    LinkedList
+    public void showList()   {
+        Node position = head;
+        while (position != null)    {
+            System.out.println(position.getStuff());
+            position = position.getLink();
+        }
+    } //end of showList() method
 
+    public int length()    {
+        int count = 0;
+        Node position = head;
+        while (position != null)    {
+            count++;
+            position = position.getLink();
+        }
+        return count;
+    } //end of length() method
 
- //      Node test = new Node();
-  //      System.out.println(test.getLink());
+    public void addToList(String stuff)  {
+        head = new Node(stuff, head);
+    } //end of addToList() method
 
- //      test.setStuff("bigBang");
-  //      System.out.println(test.getStuff());
-   Node head = new
+    public void deleteFirstNode() {
+        if (head != null)
+            head = head.getLink();
+        else {
+            System.out.println("List is empty");
+            System.exit(0);
+        }
     }
 
 
