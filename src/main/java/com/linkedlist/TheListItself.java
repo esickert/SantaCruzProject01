07fb98d8cd14c61f65ff8fea724c3f33365a08ad
Erich Sickert <esickert@gmail.com>
2018-02-17 09:45:44 -0800

    plus

--- src/main/java/com/linkedlist/TheListItself.java
+++ src/main/java/com/linkedlist/TheListItself.java
@@ -10,10 +10,9 @@ public class TheListItself {
     //list.addToList("junk");
     System.out.println(list.length());
     list.printList();
-    //for(int i = 0; i <= 3; i++) {
-    //v      list.addToList("more junk");
-
-  //  }
+    for(int i = 0; i <= 3; i++) {
+          list.addToList("more junk");
+    }
     list.printList();
     System.out.println("The number of nodes is " + list.numberOfNodes);
     System.out.println(list.length());
